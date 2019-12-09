0acca23dc0002b33cfe9c582a6944b5c67423034
Hilmar Orth <hm.orth@aol.com>
2019-12-09 11:09:08 +0100

    prior to router

---
+++ src/pages/Create.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Container from '@material-ui/core/Container';
+
+
+export default function Create() {
+    return (
+        <Container style={{background: "white"}} maxWidth="lg">
+            {// Input Stepper
+            }
+        </Container>
+
+
+
+    )
+}
